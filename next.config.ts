933a8d31d69505bd592488a1fa81b9e8b6facfcc
rafaellamonier21 <rafael.lamonier@somosuni.com.br>
2025-04-04 19:30:41 -0300

    adjust next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig: import("next").NextConfig = {
 	output: "export",
 	// Opcional: Configure o basePath se o seu site estiver em um subdiretório do GitHub Pages
-	// basePath: '/portfolio',
+	basePath: "/portfolio",
 	images: {
 		unoptimized: true, // Necessário para exportação estática com imagens não otimizadas
 	},
